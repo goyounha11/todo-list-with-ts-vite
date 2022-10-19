4f4acb86e4330ad622b942d4273c59e2d4ed593b
jjuro <chulho.shin@cubewiz.co.kr>
2022-10-19 15:11:46 +0900

    린트 분석

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,11 @@ module.exports = {
   parser: "vue-eslint-parser",
   // 코드를 분석할 수 있는 분석기
   parserOptions: {
+    // 디폴트는 script , ecmascript(es6)라면 module
     sourceType: "module",
+    // ecma버전
     ecmaVersion: "latest",
+    // 종류가 Esprima, babel, typescript 있는데 타입스크립트로 지정한다
     parser: "@typescript-eslint/parser",
     ecmaFeatures: {
       globalReturn: false,
@@ -47,17 +50,29 @@ module.exports = {
         ? ["error", { allow: ["warn", "error"] }]
         : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // 문자열은 항상 쌍따옴표
     quotes: ["error", "double"],
+    // 세미콜론 안 붙이면 항상 에러
     semi: ["error", "always"],
-    "comma-dangle": "off", // 마지막 , 찍어야함
+    // 마지막 , 찍어야함
+    "comma-dangle": "off",
+    // 조건에서 정확하지 않은 연산은 항상 에러로 표시
     "no-cond-assign": ["error", "always"],
+    // 익명함수 뒤에 공백허용, 그 외 함수 공백허용 안함
     "space-before-function-paren": "off",
+    //들여쓰기 검사 끔
     indent: "off",
+    // eslint내에서 prettier가 돌아갈 때 prettier 규칙검사 끔
     "prettier/prettier": "off",
+    //클래스안 프로퍼티 사이 공백 검사
     "lines-between-class-members": "off",
+    // 컴포넌트안에 v-text, v-html 사용하는지 검사
     "vue/no-v-text-v-html-on-component": "off",
+    // 한줄에 여러개의 프로퍼티 나열하지 않도록 검사
     "vue/max-attributes-per-line": "off",
+    // 한줄에 html tag 여러개 쓰지 않도록 검사
     "vue/singleline-html-element-content-newline": "off",
+    // 유효한 단일 루트 요소가 포함되어 있는지 확인?
     "vue/no-multiple-template-root": 0,
   },
 };
